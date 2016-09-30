d55e14ecccf951570569222bd0e758400a8cf927
Rigel Preston <rigelpreston@RigelPrestonMBP.local>
2016-09-30 16:38:45 -0400

    IT WORKS NOW

--- gOOgle.js
+++ gOOgle.js
@@ -1,27 +1,40 @@
- $(function () {
+$(function () {
     $("#suBmit").click(function (ev) {
-        var zipCode = $("#zip").val();
-        getFromGoogle(zipCode);
+
+        getFromGoogle();
         $("#zip").val("");
+
     });
-    $("#clEar").click(function(){
-        location.reload();
-    })
+    
 
-});
+})
 
-var getFromGoogle = function (zipCode) {
-    var googleKey = "AIzaSyDddtmAMdXoJlhu5DfiirduPfHp-WdbMtI";
-    var google = "https://maps.googleapis.com/maps/api/js?key=" + googleKey + " & " + darkSky + "=" + "initMap"
+var getFromGoogle = function () {
+    var zipCode = $("#zip").val();
+    //var googleKey = "&key=AIzaSyBMBe4SWYwo0lxpHh7abF8zTQEqPzOdojw";
+    var google = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + "&key=AIzaSyBPY4LKbTgzhE9wjzBw_zx9_p98y1A7PKo";
 
- getFromDarkSky(lati, lon);
+
+    $.ajax(google).done(function (data) {
+
+        var lati = data.results[0].geometry.location.lat;
+        var lon = data.results[0].geometry.location.lng;
+        console.log(lati, lon);
+
+        initMap(lati, lon);
+
     });
 }
+var initMap = function (lati, lon) {
+    var myLatLng = { lat: + lati, lng: + lon };
 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: myLatLng
+    })
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map
 
-var getFromDarkSky = function (latitude, longitude, nameCity) {
-    var darkSkyKey = "3cb454feee3d28662007fe5bef209096";
-    var darkSky = "https://api.darksky.net/forecast/" + darkSkyKey + "/" + latitude + "," + longitude;
-    $.ajax(darkSky, { dataType: "jsonp" }).done(function (data) {
-
-    };
+    })
+}
